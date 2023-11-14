6ad326529e84d1efcb398a9ed4fa0dcf4884cfdc
bhawnaniankit <bhawnaniankit@gmail.com>
2023-11-14 12:20:14 +0530

    Final Commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pandas
 NATO=pandas.read_csv("./NATO_Alphabet/nato_phonetic_alphabet.csv")
 words={row.letter:row.code for (index,row) in NATO.iterrows()}
-
 n=input("Enter a word").upper()
 NATO_words=[words[letter] for letter in n]
 print(NATO_words)
