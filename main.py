58e09f0340361a70f2c53a0b5ca202d9fb405a0a
bhawnaniankit <bhawnaniankit@gmail.com>
2023-11-16 16:53:40 +0530

    Exceptions Handeled

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import pandas
 NATO=pandas.read_csv("./NATO_Alphabet/nato_phonetic_alphabet.csv")
 words={row.letter:row.code for (index,row) in NATO.iterrows()}
-n=input("Enter a word").upper()
-NATO_words=[words[letter] for letter in n]
-print(NATO_words)
+
+def generate():
+    n=(input("Enter a word\n").upper()).strip()
+    try:
+        NATO_words=[words[letter] for letter in n]
+    except KeyError:
+        print("Enter a valid letters")
+        generate()
+    else:
+        print((NATO_words))
+
+generate()
